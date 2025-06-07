9bc833d13df474c4a0f9c68d695f06bc45fdb057
brayan otieno <briandan2058@gmail.com>
2025-06-07 21:12:23 +0300

    Add custom password complexity validator and enforce secure password rules

--- base/settings.py
+++ base/settings.py
@@ -111,6 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 8}, 
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -118,7 +119,11 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'base.validators.ComplexPasswordValidator',
+    },
 ]
+
 
 
 # Internationalization
